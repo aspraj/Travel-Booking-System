08690bb3020387afce81f0b3f5b489479a525751
aman singh <amansinghpatel429@gmail.com>
2026-04-12 18:20:00 +0530

    Phase 1: User registration
    Phase 1: User registration with Spring Boot, MySQL, Swagger, Security setup

---
+++ User Service/src/main/java/User/Service/main/MainApplication.java
@@ -0,0 +1,13 @@
+package User.Service.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
---
+++ User Service/src/main/java/User/Service/main/utils/JwtUtil.java
@@ -0,0 +1,33 @@
+package User.Service.main.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private static final String SECRET =
+            "travel-booking-system-super-secure-key-2026!!";
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 hour
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
